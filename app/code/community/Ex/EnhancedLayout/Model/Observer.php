aef82809d82478f528618cdaacd18bcdc9edb140
Suky <suky3plex@outlook.com>
2015-04-10 18:28:40 +0200

    Added logging option for Ex_EnhancedLayout module; Code refactoring;

--- app/code/community/Ex/EnhancedLayout/Model/Observer.php
+++ app/code/community/Ex/EnhancedLayout/Model/Observer.php
@@ -11,18 +11,42 @@ class Ex_EnhancedLayout_Model_Observer
 {
 
     /**
+     * @var bool
+     */
+    private $_debug = false;
+
+    /**
+     * @param $output
+     * @param $file
+     */
+    private function _log($output, $file)
+    {
+        return Mage::log($output, Zend_Log::DEBUG, $file, true);
+    }
+
+    /**
+     * Removes CATEGORY_XXX and PRODUCT_XXX handles
+     * @note: Widgets for specific categories and specific products WILL NOT WORK!
      * @param Varien_Event_Observer $observer
      */
     public function optimizeLayout(Varien_Event_Observer $observer)
     {
         /** @var Mage_Core_Model_Layout_Update $update */
         $update = $observer->getEvent()->getLayout()->getUpdate();
+        /** log before changes applied */
+        if($this->_debug){
+            $this->_log($update->getHandles(), 'handles_before.log');
+        }
 
         foreach($update->getHandles() as $handle){
             if( strpos($handle, 'CATEGORY_') === 0
                 || ( strpos($handle, 'PRODUCT_') === 0 && strpos($handle, 'PRODUCT_TYPE_') === false)){
                 $update->removeHandle($handle);
             }
+        }
+        /** log after changes applied */
+        if($this->_debug){
+            $this->_log($update->getHandles(), 'handles_after.log');
         }
     }
 }
