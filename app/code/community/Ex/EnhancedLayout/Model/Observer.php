8ed2d8b25f79118964b65f6a7d7122b01a0e052b
Suky <suky3plex@outlook.com>
2015-04-10 17:35:55 +0200

    Added Ex_EnhancedLayout module

---
+++ app/code/community/Ex/EnhancedLayout/Model/Observer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class Ex_EnhancedLayout_Model_Observer
+ *
+ * @category    Ex
+ * @package     Ex_EnhancedLayout
+ * @author      Suky <suky3plex@outlook.com> (http://ex.pendabl.es)
+ * @license     http://unlicense.org  Unlicensed Free Software
+ */
+class Ex_EnhancedLayout_Model_Observer
+{
+
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function optimizeLayout(Varien_Event_Observer $observer)
+    {
+        $update = $observer->getLayout()->getUpdate();
+
+        foreach($update->getHandles() as $handle){
+            if( strpos($handle, 'CATEGORY_') === 0
+                || ( strpos($handle, 'PRODUCT_') === 0 && strpos($handle, 'PRODUCT_TYPE_') === false)){
+                $update->removeHandle($handle);
+            }
+        }
+    }
+}
